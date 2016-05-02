7f8ce9908c91220abad6f11731f61191c3bf5fb7
J-Weeks <weeksjasons@gmail.com>
2016-05-02 12:27:55 -0400

    Adds Mocha tests
    alters jshint
    changes aliases to mochaTest
    removes files that ref jasmine
    adds solutions to test mocha
    updates package.json
    adds correct spec in mocha
    adds chai

--- spec/diagnostic.spec.js
+++ spec/diagnostic.spec.js
@@ -1,57 +1,53 @@
 'use strict';
 
+const chai = require('chai');
+const expect = chai.expect;
+
+// const assert = require('assert');
 const assessment = require('../lib/diagnostic.js');
 
-describe('reduce', function() {
-
-  var array = [-1, -2, -3, -4, -5];
-
-  describe('sum', function() {
-
-    it('returns the sum of the array', function() {
-      expect(array.reduce(assessment.sum)).toBe(-15);
+describe('Reduce', function() {
+  let array = [-1, -2, -3, -4, -5];
+    describe('sum', function () {
+      it('returns the sum of the array', function () {
+        expect(array.reduce(assessment.sum)).to.equal(-15);
+      });
     });
 
-  });
+// above working
 
   describe('min', function() {
-
     it('returns the minimum of the array elements', function() {
-      expect(array.reduce(assessment.min)).toBe(-5);
+      expect(array.reduce(assessment.min)).to.equal(-5);
     });
-
   });
-
 });
 
+
+//reduce end
+
 describe('enumerable', function() {
-
-  var array = [-1, -2, -3, -4, -5];
-
+let array = [-1, -2, -3, -4, -5];
   describe('any', function() {
-
     it('returns false for greater than 0 predicate', function() {
-      expect(assessment.any(array, n => n > 0)).toBe(false);
+      expect(assessment.any(array, n => n > 0)).to.equal(false);
     });
 
     it('returns true for less than 0 predicate', function() {
-      expect(assessment.any(array, n => n < 0)).toBe(true);
+      expect(assessment.any(array, n => n < 0)).to.equal(true);
     });
 
     it('returns true for equal -3 predicate', function() {
-      expect(assessment.any(array, n => n === -3)).toBe(true);
+      expect(assessment.any(array, n => n === -3 )).to.equal(true);
     });
-
   });
 
   describe('select', function() {
-
-    var answer = [-1, -3, -5];
-
+    let answer = [-1, -3, -5];
     it('returns correct array for not even predicate', function() {
-      expect(assessment.select(array, n => n % 2)).toEqual(answer);
+      expect(assessment.select(array, n => n % 2)).to.deep.equal(answer);
+      // expect(assessment.select(array, n => n % 2)).to.deep.equal(answer);
     });
-
   });
 
   describe('first', function() {
@@ -66,16 +62,16 @@ describe('enumerable', function() {
       name: 'repeat',
     }, {
       name: 'fifth',
-    }, ]; // jscs: ignore
+    }, ];
 
     it('returns correct object in array', function() {
       expect(assessment.first(withDuplicates,
-        o => o.name === 'repeat')).toBe(withDuplicates[1]);
+        o => o.name === 'repeat')).to.equal(withDuplicates[1]);
     });
 
     it('returns undefined for unmatched object', function() {
       expect(assessment.first(withDuplicates,
-        o => o.name === 'fourth')).toBeUndefined();
+        o => o.name === 'fourth')).to.equal(undefined);
     });
 
   });
